87b7852cb22220888faad00794dd57a7e23352ff
Florian Pieper <florian@pieper.ai>
2023-08-02 15:14:04 +0200

    Add example blueprint how to use BalancedDecimal

---
+++ examples/balanced_decimal/src/lib.rs
@@ -0,0 +1,37 @@
+use scrypto::prelude::*;
+use scrypto_math::balanced_decimal::*;
+
+#[blueprint]
+mod balanced_decimal_demo {
+    struct BalancedDecimalDemo {
+        oci_vault: Vault,
+    }
+
+    impl BalancedDecimalDemo {
+        pub fn instantiate() -> Global<BalancedDecimalDemo> {
+            let oci_bucket: Bucket =
+                ResourceBuilder::new_fungible(OwnerRole::None).mint_initial_supply(1000);
+            Self {
+                oci_vault: Vault::with_bucket(oci_bucket),
+            }
+            .instantiate()
+            .prepare_to_globalize(OwnerRole::None)
+            .globalize()
+        }
+
+        pub fn free_tokens_decimal(&mut self, amount: Decimal) -> (Bucket, Decimal) {
+            let amount = BalancedDecimal::try_from(amount).expect("Value too large.");
+            let output = self.oci_vault.take(amount.floor_to_decimal());
+            (output, amount.into())
+        }
+
+        pub fn free_tokens_precise_decimal(
+            &mut self,
+            amount: PreciseDecimal,
+        ) -> (Bucket, PreciseDecimal) {
+            let amount = BalancedDecimal::try_from(amount).expect("Value too large.");
+            let output = self.oci_vault.take(amount.ceil_to_decimal());
+            (output, amount.into())
+        }
+    }
+}
