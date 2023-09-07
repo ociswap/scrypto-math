00eee25b9574f2628cbe5308ab5190c9266da1c1
Florian Pieper <florian@pieper.ai>
2023-09-07 12:54:41 +0200

    Migrate logarithm - one failing test

--- src/logarithm.rs
+++ src/logarithm.rs
@@ -1,76 +1,73 @@
 use num_traits::Zero;
-use radix_engine_common::dec;
 use radix_engine_common::math::bnum_integer::*;
 use radix_engine_common::math::decimal::*;
+use radix_engine_common::prelude::PreciseDecimal;
 use radix_engine_common::*;
 
-use crate::balanced_decimal::BalancedDecimal;
-use crate::bdec;
-
-const LN2: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    4887746961572732391,
-    3757558395076474551,
+const LN2: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    9456716947207598648,
+    37575583950764745,
     0,
     0,
 ]));
-const LN10: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    701367864101705880,
-    12482338800784407930,
+const LN10: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    5541036900753882543,
+    124823388007844079,
     0,
     0,
 ]));
-const SQRT: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    3583277511020380144,
-    7666467083416870407,
+const SQRT: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    1327104860269872414,
+    76664670834168704,
     0,
     0,
 ]));
-const SQRT_HALF: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    11015010792364965880,
-    3833233541708435203,
+const SQRT_HALF: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    663552430134936207,
+    38332335417084352,
     0,
     0,
 ]));
 
-const LG1: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    7480962735871623168,
-    3614007241618385227,
+const LG1: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    5055430527260295168,
+    36140072416183852,
     0,
     0,
 ])); // 6.666666666666735130e-01
-const LG2: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    3510068727102046208,
-    2168404344938993412,
+const LG2: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    2248709976116166656,
+    21684043449389934,
     0,
     0,
 ])); // 3.999999999940941908e-01
-const LG3: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    8437904063097995264,
-    1548860255744677571,
+const LG3: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    13181567332964761600,
+    15488602557446775,
     0,
     0,
-])); // 2.857142874366239149e-01; /* 3FD24924 94229359 */
-const LG4: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    10113378558716936192,
-    1204667790877038313,
+])); // 2.857142874366239149e-01
+const LG4: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    2499210515169411072,
+    12046677908770383,
     0,
     0,
 ])); // 2.222219843214978396e-01
-const LG5: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    4675241628135325696,
-    985733422058661494,
+const LG5: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    17386691845568331776,
+    9857334220586614,
     0,
     0,
 ])); // 1.818357216161805012e-01
-const LG6: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    866527560993865728,
-    830164805128661067,
+const LG6: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    12367983804995338240,
+    8301648051286610,
     0,
     0,
-])); // 1.531383769920937332e-01;
-const LG7: BalancedDecimal = BalancedDecimal(BnumI256::from_digits([
-    13792263540114456576,
-    802211953826968189,
+])); // 1.531383769920937332e-01
+const LG7: PreciseDecimal = PreciseDecimal(I256::from_digits([
+    16555524861002645504,
+    8022119538269681,
     0,
     0,
 ])); // 1.479819860511658591e-01
@@ -81,73 +78,73 @@ pub trait LogarithmDecimal {
     fn log_base(&self, base: Decimal) -> Option<Decimal>;
 }
 
-pub trait LogarithmBalancedDecimal {
-    fn ln(&self) -> Option<BalancedDecimal>;
-    fn log2(&self) -> Option<BalancedDecimal>;
-    fn log10(&self) -> Option<BalancedDecimal>;
-    fn log_base(&self, base: BalancedDecimal) -> Option<BalancedDecimal>;
+pub trait LogarithmPreciseDecimal {
+    fn ln(&self) -> Option<PreciseDecimal>;
+    fn log2(&self) -> Option<PreciseDecimal>;
+    fn log10(&self) -> Option<PreciseDecimal>;
+    fn log_base(&self, base: PreciseDecimal) -> Option<PreciseDecimal>;
 }
 
-fn log_reduce_argument(number: BalancedDecimal) -> (i32, BalancedDecimal) {
-    let full_integer = number.0 / BalancedDecimal::ONE.0;
+fn log_reduce_argument(number: PreciseDecimal) -> (i32, PreciseDecimal) {
+    let full_integer = number.0 / PreciseDecimal::ONE.0;
 
     if full_integer.is_zero() {
         if number >= SQRT_HALF {
             return (0, number);
         }
         let k = number.0.leading_zeros() as i32 - SQRT_HALF.0.leading_zeros() as i32;
-        let r = number * BalancedDecimal(BalancedDecimal::ONE.0 << BnumI256::from(k));
+        let r = number * PreciseDecimal(PreciseDecimal::ONE.0 << k as u32);
 
         if r >= SQRT {
             return (-k, r);
         }
 
-        return (-k - 1, r * dec!(2));
+        return (-k - 1, r * pdec!(2));
     }
 
     let k = 255 - full_integer.leading_zeros() as i32; // index highest integer bit
-    let r = number / BalancedDecimal(BalancedDecimal::ONE.0 << BnumI256::from(k));
+    let r = number / PreciseDecimal(PreciseDecimal::ONE.0 << k as u32);
 
     if r <= SQRT {
         return (k, r);
     }
 
-    return (k + 1, r / dec!(2));
+    return (k + 1, r / pdec!(2));
 }
 
 impl LogarithmDecimal for Decimal {
     fn ln(&self) -> Option<Decimal> {
-        BalancedDecimal::try_from(*self)
+        PreciseDecimal::try_from(*self)
             .ok()?
             .ln()
-            .map(|log| log.into())
+            .and_then(|log| log.try_into().ok())
     }
 
     fn log2(&self) -> Option<Decimal> {
-        BalancedDecimal::try_from(*self)
+        PreciseDecimal::try_from(*self)
             .ok()?
             .log2()
-            .map(|log| log.into())
+            .and_then(|log| log.try_into().ok())
     }
 
     fn log10(&self) -> Option<Decimal> {
-        BalancedDecimal::try_from(*self)
+        PreciseDecimal::try_from(*self)
             .ok()?
             .log10()
-            .map(|log| log.into())
+            .and_then(|log| log.try_into().ok())
     }
 
     fn log_base(&self, base: Decimal) -> Option<Decimal> {
-        let base = BalancedDecimal::try_from(base).ok()?;
-        BalancedDecimal::try_from(*self)
+        let base = PreciseDecimal::try_from(base).ok()?;
+        PreciseDecimal::try_from(*self)
             .ok()?
             .log_base(base)
-            .map(|log| log.into())
+            .and_then(|log| log.try_into().ok())
     }
 }
 
-impl LogarithmBalancedDecimal for BalancedDecimal {
-    fn ln(&self) -> Option<BalancedDecimal> {
+impl LogarithmPreciseDecimal for PreciseDecimal {
+    fn ln(&self) -> Option<PreciseDecimal> {
         // based on https://github.com/rust-lang/libm/blob/master/src/math/log.rs
         if !self.is_positive() {
             return None;
@@ -157,23 +154,23 @@ impl LogarithmBalancedDecimal for BalancedDecimal {
         // println!("x_n = {:?}", pdec!(2).powi(k.into()) * r);
         // println!("x_o = {:?}", self);
 
-        let f = r - BalancedDecimal::ONE;
-        let s = f / (bdec!(2) + f);
+        let f = r - PreciseDecimal::ONE;
+        let s = f / (pdec!(2) + f);
         let z = s * s;
         let w = z * z;
         let remez = z * (LG1 + w * (LG3 + w * (LG5 + w * LG7))) + w * (LG2 + w * (LG4 + w * LG6));
         Some(LN2 * k + f - s * (f - remez))
     }
 
-    fn log2(&self) -> Option<BalancedDecimal> {
+    fn log2(&self) -> Option<PreciseDecimal> {
         Some(self.ln()? / LN2)
     }
 
-    fn log10(&self) -> Option<BalancedDecimal> {
+    fn log10(&self) -> Option<PreciseDecimal> {
         Some(self.ln()? / LN10)
     }
 
-    fn log_base(&self, base: BalancedDecimal) -> Option<BalancedDecimal> {
+    fn log_base(&self, base: PreciseDecimal) -> Option<PreciseDecimal> {
         let base_ln = base.ln()?;
         Some(self.ln()? / base_ln)
     }
@@ -185,16 +182,31 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
+    fn test_constants() {
+        assert_eq!(LN2, pdec!("0.693147180559945309417232121458176568"));
+        assert_eq!(LN10, pdec!("2.302585092994045684017991454684364207"));
+        assert_eq!(SQRT, pdec!("1.414213562373095048801688724209698078"));
+        assert_eq!(SQRT_HALF, pdec!("0.707106781186547524400844362104849039"));
+        assert_eq!(LG1, pdec!("0.6666666666666735130"));
+        assert_eq!(LG2, pdec!("0.3999999999940941908"));
+        assert_eq!(LG3, pdec!("0.2857142874366239149"));
+        assert_eq!(LG4, pdec!("0.2222219843214978396"));
+        assert_eq!(LG5, pdec!("0.1818357216161805012"));
+        assert_eq!(LG6, pdec!("0.1531383769920937332"));
+        assert_eq!(LG7, pdec!("0.1479819860511658591"));
+    }
+
+    #[test]
     fn test_ln_positive_number() {
         assert_eq!(
             dec!(10).ln(),
             Some(dec!("2.302585092994045684") - dec!("0.000000000000000001"))
         );
         assert_eq!(
-            bdec!(10).ln(),
+            pdec!(10).ln(),
             Some(
-                bdec!("2.30258509299404568401799145468436420760")
-                    - bdec!("0.00000000000000000009315192671654058382")
+                pdec!("2.302585092994045684017991454684364207")
+                    - pdec!("0.000000000000000000093151926716540583")
             )
         );
     }
@@ -206,27 +218,27 @@ mod tests {
             Some(dec!(1) - dec!("0.000000000000000001"))
         );
         assert_eq!(
-            bdec!("2.71828182845904523536028747135266249775").ln(),
-            Some(bdec!(1) - bdec!("0.00000000000000000007225640213908820418"))
+            pdec!("2.718281828459045235360287471352662497").ln(),
+            Some(pdec!(1) - pdec!("0.000000000000000000072256402139088204"))
         );
     }
 
     #[test]
     fn test_ln_one() {
         assert_eq!(dec!(1).ln(), Some(dec!(0)));
-        assert_eq!(bdec!(1).ln(), Some(bdec!(0)));
+        assert_eq!(pdec!(1).ln(), Some(pdec!(0)));
     }
 
     #[test]
     fn test_ln_zero() {
         assert_eq!(dec!(0).ln(), None);
-        assert_eq!(bdec!(0).ln(), None);
+        assert_eq!(pdec!(0).ln(), None);
     }
 
     #[test]
     fn test_ln_negative_number() {
         assert_eq!(dec!(-1).ln(), None);
-        assert_eq!(bdec!(-1).ln(), None);
+        assert_eq!(pdec!(-1).ln(), None);
     }
 
     #[test]
@@ -236,10 +248,10 @@ mod tests {
             Some(dec!("69.077552789821370520"))
         );
         assert_eq!(
-            bdec!("1000000000000000000000000000000").ln(),
+            pdec!("1000000000000000000000000000000").ln(),
             Some(
-                bdec!("69.07755278982137052053974364053092622803")
-                    + bdec!("0.00000000000000000027411142136760615405")
+                pdec!("69.077552789821370520539743640530926228")
+                    + pdec!("0.000000000000000000274111421367606147")
             )
         );
     }
@@ -251,33 +263,24 @@ mod tests {
             Some(dec!("-41.446531673892822312"))
         );
         assert_eq!(
-            bdec!("0.00000000000000000000000000000000000001").ln(),
+            pdec!("0.000000000000000000000000000000000001").ln(),
             Some(
-                bdec!("-87.49823353377373599268367527800583988885")
-                    - bdec!("0.00000000000000000002088682154503733556")
+                pdec!("-82.893063347785644624647692368637111474")
+                    + pdec!("0.000000000000000000345534790097727621")
             )
         );
     }
 
     #[test]
     fn test_ln_maximum_possible() {
+        assert_eq!(Decimal::MAX.ln(), Some(dec!("90.944579813056731786")));
         assert_eq!(
-            Decimal::from(BalancedDecimal::MAX).ln(),
-            Some(dec!("89.254297509012317908"))
-        );
-        assert_eq!(
-            BalancedDecimal::MAX.ln(),
+            PreciseDecimal::MAX.ln(),
             Some(
-                bdec!("89.25429750901231790871051569382918497041")
-                    - bdec!("0.00000000000000000002088682154503733698")
+                pdec!("93.859467695000409276746498603197913385")
+                    + pdec!("0.000000000000000000345534790097727602")
             )
         );
-    }
-
-    #[test]
-    fn test_ln_value_too_large() {
-        assert_eq!((Decimal::from(BalancedDecimal::MAX) + 1).ln(), None);
-        assert_eq!(Decimal::MAX.ln(), None);
     }
 
     #[test]
@@ -324,6 +327,9 @@ mod tests {
             dec!(20).log_base(dec!(8)),
             Some(dec!("1.440642698295787449"))
         );
-        assert_eq!(dec!(8).log_base(Decimal::MAX), None);
+        assert_eq!(
+            Decimal::MAX.log_base(dec!(8)),
+            Some(dec!("43.735098097342492579"))
+        );
     }
 }
